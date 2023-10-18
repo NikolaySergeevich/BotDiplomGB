a65212d5b50e509d42e69fe8dda9a59995c6b816
Nikolay <nik.pavlovskiy.1997@mail.ru>
2023-10-18 23:45:19 +0300

    git init and add log,configs.ini

---
+++ bot.py
@@ -0,0 +1,4 @@
+import logging
+
+# logging.basicConfig(level = logging.DEBUG)
+logging.basicConfig(format = u'%(levelname)-8s [%(asctime)s] %(message)s', level = logging.DEBUG, filename = u'D:/Учёба в GB/Диплом/mylog.log')
