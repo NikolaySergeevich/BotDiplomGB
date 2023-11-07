8c9a9ea5e4e6597cd5bc8e5f7997baf50b2b3337
Nikolay <nik.pavlovskiy.1997@mail.ru>
2023-11-07 20:35:45 +0300

    add func get_graf_compare

--- bot.py
+++ bot.py
@@ -521,26 +521,68 @@ async def answer_for_time_control(call: types.CallbackQuery):
 async def start(message: types.Message):
     with open(t.get_way_of_img('analist'), 'rb') as photo:
         await bot.send_photo(chat_id=message.from_user.id, photo = photo)
+@dp.message_handler(commands=['get_graf_compare_with_analist'])
+async def start(message: types.Message):
+    try:
+        with open(t.get_way_of_img_compare(message.from_user.id, 'analist'), 'rb') as photo:
+            await bot.send_photo(chat_id=message.from_user.id, photo = photo)
+    except FileNotFoundError:
+        text = "Нечего сравнивать. Сразу пройдите тест, а затем смотрите графики срвнения."
+        await bot.send_message(message.from_user.id, text)
+
 
 @dp.message_handler(commands=['get_graf_developer'])
 async def start(message: types.Message):
     with open(t.get_way_of_img('developer'), 'rb') as photo:
         await bot.send_photo(chat_id=message.from_user.id, photo = photo)
+@dp.message_handler(commands=['get_graf_compare_with_developer'])
+async def start(message: types.Message):
+    try:
+        with open(t.get_way_of_img_compare(message.from_user.id, 'developer'), 'rb') as photo:
+            await bot.send_photo(chat_id=message.from_user.id, photo = photo)
+    except FileNotFoundError:
+        text = "Нечего сравнивать. Сразу пройдите тест, а затем смотрите графики срвнения."
+        await bot.send_message(message.from_user.id, text)
 
 @dp.message_handler(commands=['get_graf_tester']) 
 async def start(message: types.Message):
     with open(t.get_way_of_img('tester'), 'rb') as photo:
         await bot.send_photo(chat_id=message.from_user.id, photo = photo)
+@dp.message_handler(commands=['get_graf_compare_with_tester'])
+async def start(message: types.Message):
+    try:
+        with open(t.get_way_of_img_compare(message.from_user.id, 'tester'), 'rb') as photo:
+            await bot.send_photo(chat_id=message.from_user.id, photo = photo)
+    except FileNotFoundError:
+        text = "Нечего сравнивать. Сразу пройдите тест, а затем смотрите графики срвнения."
+        await bot.send_message(message.from_user.id, text)
+
 
 @dp.message_handler(commands=['get_graf_project'])
 async def start(message: types.Message):
     with open(t.get_way_of_img('project'), 'rb') as photo:
         await bot.send_photo(chat_id=message.from_user.id, photo = photo)
+@dp.message_handler(commands=['get_graf_compare_with_project'])
+async def start(message: types.Message):
+    try:
+        with open(t.get_way_of_img_compare(message.from_user.id, 'project'), 'rb') as photo:
+            await bot.send_photo(chat_id=message.from_user.id, photo = photo)
+    except FileNotFoundError:
+        text = "Нечего сравнивать. Сразу пройдите тест, а затем смотрите графики срвнения."
+        await bot.send_message(message.from_user.id, text)
 
 @dp.message_handler(commands=['get_graf_prodact'])
 async def start(message: types.Message):
     with open(t.get_way_of_img('prodact'), 'rb') as photo:
-        await bot.send_photo(chat_id=message.from_user.id, photo = photo)    
+        await bot.send_photo(chat_id=message.from_user.id, photo = photo) 
+@dp.message_handler(commands=['get_graf_compare_with_prodact'])
+async def start(message: types.Message):
+    try:
+        with open(t.get_way_of_img_compare(message.from_user.id, 'prodact'), 'rb') as photo:
+            await bot.send_photo(chat_id=message.from_user.id, photo = photo)
+    except FileNotFoundError:
+        text = "Нечего сравнивать. Сразу пройдите тест, а затем смотрите графики срвнения."
+        await bot.send_message(message.from_user.id, text)   
 
 
 
